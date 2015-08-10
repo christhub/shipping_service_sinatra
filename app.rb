768f57e1c18e9751a66f787c78db4541eeac92fb
Leah Klamkin & Christian Lei <student@epicodus.com>
2015-08-10 16:48:02 -0700

    Display shipment info on price page.

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ get('/') do
 end
 
 get('/parcels') do
-  length = params.fetch('length').to_i
-  width = params.fetch('width').to_i
-  height = params.fetch('height').to_i
-  weight = params.fetch('weight').to_i
-  gift_wrap_option = params.fetch('gift_wrap')
-  price_to_ship = Parcel.new(length, width, height, weight, gift_wrap_option).cost_to_ship()
-  price_to_wrap = Parcel.new(length, width, height, weight, gift_wrap_option).gift_wrap?()
+  @length = params.fetch('length').to_i
+  @width = params.fetch('width').to_i
+  @height = params.fetch('height').to_i
+  @weight = params.fetch('weight').to_i
+  @gift_wrap_option = params.fetch('gift_wrap')
+  price_to_ship = Parcel.new(@length, @width, @height, @weight, @gift_wrap_option).cost_to_ship()
+  price_to_wrap = Parcel.new(@length, @width, @height, @weight, @gift_wrap_option).gift_wrap?()
   @price_to_transport = '%.2f' % (price_to_ship + price_to_wrap)
   erb(:price)
 end
