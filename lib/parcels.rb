fd066b187b9a2810a3c7d840819d4b73541e72a4
Leah Klamkin & Christian Lei <student@epicodus.com>
2015-08-10 11:55:51 -0700

    Calculate shipping cost based on weight and volume.

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -6,8 +6,22 @@ class Parcel
     @weight = weight
   end
 
-  define_method(:parcel) do
-    volume = @length * @width * @height
-    return volume
+  define_method(:volume) do
+    @volume = @length * @width * @height
+    return @volume
+  end
+
+  define_method(:cost_to_ship) do
+    if (@weight <= 5) && ((@length < 48) || (@width < 48) || (@height < 48))
+      "Free shipping!"
+    elsif ((@weight > 5) && (@weight <= 20)) && ((@length < 48) || (@width < 48) || (@height < 48))
+      "$14.95 shipping"
+    elsif
+      ((@weight > 20) && (@weight <= 100)) && ((@length < 48) || (@width < 48) || (@height < 48))
+      "$24.95 shipping"
+    elsif
+      (@weight > 100) && ((@length < 48) || (@width < 48) || (@height < 48))
+      "$39.95 shipping"
+    end
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -2,10 +2,17 @@ require('rspec')
 require('parcels')
 
 describe(Parcel) do
-  describe('#parcel') do
+  describe('#volume') do
     it ('will return the product of the dimensions to determine volume') do
       new_parcel = Parcel.new(3, 3, 3, 5)
-      expect(new_parcel.parcel()).to(eq(27))
+      expect(new_parcel.volume()).to(eq(27))
+    end
+  end
+
+  describe('#cost_to_ship') do
+    it ('returns the shipping cost of the parcel') do
+      new_parcel = Parcel.new(3, 3, 3, 20)
+      expect(new_parcel.cost_to_ship()).to(eq("$14.95 shipping"))
     end
   end
 end
